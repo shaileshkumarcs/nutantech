2564f782dfb5b84a672f7d7fde1454d1600b3ca6
Shailesh Kumar Dangi <shailesh.kumarcs@gmail.com>
2018-07-19 15:33:47 +0530

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -3,10 +3,10 @@
 	$config['smtp_host'] = 'smtp.gmail.com';
 	$config['smtp_port'] = '587';
 	$config['smtp_crypto'] = 'tls';
-	//$config['smtp_user'] = 'deepika.verma@wayfers.com';
-	//$config['smtp_pass'] = 'TY@Universe';
-	$config['smtp_user'] = 'shailesh94kumar@gmail.com';
-	$config['smtp_pass'] = '9939744337';
+	//$config['smtp_user'] = 'xxxxxx.verma@wayfers.com';
+	//$config['smtp_pass'] = 'xxxxx';
+	$config['smtp_user'] = 'xxxxxx@gmail.com';
+	$config['smtp_pass'] = 'xxxxxx';
 	$config['charset'] = 'utf-8';
 	$config['mailtype'] = 'html';
 	$config['newline'] = "\r\n";*/
@@ -16,8 +16,8 @@
 	$config['smtp_host'] = 'smtp.gmail.com';
 	$config['smtp_port'] = '587';
 	$config['smtp_crypto'] = 'tls';
-	$config['smtp_user'] = 'contact.nutantech@gmail.com';
-	$config['smtp_pass'] = 'nutantech@123';
+	$config['smtp_user'] = 'xxxxx.xxxxx@gmail.com';
+	$config['smtp_pass'] = 'xxxxxxx@123';
 	$config['charset'] = 'utf-8';
 	$config['mailtype'] = 'html';
 	$config['newline'] = "\r\n";
